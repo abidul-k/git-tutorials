c7ea39d738510b7fe1a81f683fb1476a39879c60
a_kabir <abidul.kabir.7@gmail.com>
2021-10-24 17:13:52 +0600

    Added new lines

--- hello.java
+++ hello.java
@@ -1,5 +1,15 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("VS Code is just a awesome thing....");
+        System.out.println("VS Code is all about something joss....");
     }
+
+    joss("HM");
+
+public static void joss(String name) {
+    System.out.println("Bla bla...." + name);
+}
+
+public.static int sqrt(int a) {
+    return a*a;
+}
 }
